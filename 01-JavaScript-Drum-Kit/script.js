a98685e222e7122c673ffc6065a6f67fdbc0b4fe
Steve Richards <steve.richards@gmail.com>
2017-08-13 19:40:50 -0700

    Day 1 challenge finished

---
+++ 01-JavaScript-Drum-Kit/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('keydown', playSound);
+
+keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function playSound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
